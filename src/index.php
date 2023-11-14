1e74bada4fd6bebc5a4a3a075e676e575d8d42b4
Christabellet <christabellethamm@gmail.com>
2023-11-14 15:51:28 +0800

    a

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@ session_start();
 
 function isCommonPassword($password) {
     // Define an array of common passwords from your txt file
-    $commonPasswords = file('/10-million-password-list-top-1000.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $commonPasswords = file('10-million-password-list-top-1000.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
     echo "Pw: $commonPasswords";
     // Check if the provided password is in the common passwords list
